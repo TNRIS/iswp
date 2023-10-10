d253c821abf71d9b71d0b0a520ecf6bbf6eb02b3
lh <111371248+L-Har@users.noreply.github.com>
2023-10-10 15:44:05 -0500

    use helper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,39 +2,18 @@
     //@ts-nocheck
     import PopulationChart from "../components/Charts/PopulationChart.svelte";
     import DataUsageType from "../components/DataUsageType.svelte";
-    import TitleBlurb from "../components/TitleBlurb.svelte";
-    import ThemeTypesByDecadeChart from "../components/ThemeTypesByDecadeChart.svelte";
-    import ThemeTotalsByDecadeChart from "../components/ThemeTotalsByDecadeChart.svelte";
-    import DataViewChoiceWrap from "../components/DataByPlanningDecadeAndTheme/DataViewChoiceWrap.svelte";
-    import { start_all_db } from "../lib/db/db.js";
-    import { onMountSync } from "/src/lib/helper.js";
+    import TitleBlurb from "/src/components/TitleBlurb.svelte";
+    import ThemeTypesByDecadeChart from "/src/components/ThemeTypesByDecadeChart.svelte";
+    import ThemeTotalsByDecadeChart from "/src/components/ThemeTotalsByDecadeChart.svelte";
+    import DataViewChoiceWrap from "/src/components/DataByPlanningDecadeAndTheme/DataViewChoiceWrap.svelte";
+    import { start_all_db } from "$lib/db/db.js";
+    import { load_indexeddb } from "$lib/helper.js";
 
-    // Build the indexedDB
-    let build_indexeddb = async () => {
-        await onMountSync();
-
-        let IS_2022_WEBSITE = window.location.href.indexOf("2022") > -1;
-        let IS_2017_WEBSITE = window.location.href.indexOf("2017") > -1;
-
-        // Start indexeddb, efficient if upgrade is not needed.
-        let [db17, db22] = await start_all_db();
-
-        // Choose database depending on url.
-        if (!(db22 && db17)) throw "Databases are null.";
-
-        if (IS_2022_WEBSITE) {
-            return db22;
-        } else if (IS_2017_WEBSITE) {
-            return db17;
-        } else {
-            return db22;
-        }
-    };
 </script>
 
 <div class="statewide-view">
     <section>
-        {#await build_indexeddb()}
+        {#await load_indexeddb()}
             <span>Loading</span>
         {:then data}
             <PopulationChart chartTitle={"ct-pop-chart"} db={data} />
--- src/routes/county/[slug]/+page.svelte
+++ src/routes/county/[slug]/+page.svelte
@@ -1,39 +1,14 @@
 <script>
     //@ts-nocheck
     import ProjectTable from "/src/components/ProjectTable/ProjectTable.svelte";
-    import { Constant2022 } from "/src/lib/Constant2022.js";
-    const c22 = new Constant2022();
     export let data;
 
-    import { start_all_db } from "/src/lib/db/db.js";
-    import { onMountSync } from "/src/lib/helper.js"
-
-    // Build the indexedDB
-    let build_indexeddb = async () => {
-        await onMountSync();
-
-        let IS_2022_WEBSITE = window.location.href.indexOf("2022") > -1;
-        let IS_2017_WEBSITE = window.location.href.indexOf("2017") > -1;
-
-        // Start indexeddb, efficient if upgrade is not needed.
-        let [db17, db22] = await start_all_db();
-
-        // Choose database depending on url.
-        if (!(db22 && db17)) throw "Databases are null.";
-
-        if (IS_2022_WEBSITE) {
-            return db22;
-        } else if (IS_2017_WEBSITE) {
-            return db17;
-        } else {
-            return db22;
-        }
-    };
+    import { load_indexeddb } from "/src/lib/helper.js"
 </script>
 
 <div class="statewide-view">
     <section>
-        {#await build_indexeddb()}
+        {#await load_indexeddb()}
             <span>Loading</span>
         {:then out}
             <ProjectTable db={ out } countyFilter={ data.slug }/>
