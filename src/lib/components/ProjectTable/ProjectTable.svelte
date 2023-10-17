0b98a77396057e2168e4c84712b3754653eae5c8
lh <111371248+L-Har@users.noreply.github.com>
2023-10-17 08:35:04 -0500

    Add raw data pivot table
    Include rows
    
    Another check

--- src/lib/components/ProjectTable/ProjectTable.svelte
+++ src/lib/components/ProjectTable/ProjectTable.svelte
@@ -22,7 +22,7 @@
 
             // Need to stringify array to use includes function because [1] !== [1] since they don't have the same reference.
             if (
-                !JSON.stringify(project_data).includes(JSON.stringify(to_array))
+                !JSON.stringify(project_data).includes(JSON.stringify(to_array)) && project.WmsProjectSponsorRegion === project.WugRegion
             ) {
                 sum += project.CapitalCost;
                 project_data.push(to_array);
