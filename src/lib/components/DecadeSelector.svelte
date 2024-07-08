dbc266805ff9378fee149d5997ea9bb2345a0f8d
lh <111371248+L-Har@users.noreply.github.com>
2024-07-08 11:17:20 -0500

    aria hints for decade menubar

--- src/lib/components/DecadeSelector.svelte
+++ src/lib/components/DecadeSelector.svelte
@@ -7,22 +7,33 @@
 </script>
 
 <div class="selector decade-selector">
-    <div class="show-medium">
+    <div
+        class="show-medium"
+        role="menubar"
+        aria-label="Choose from the following to switch between decades."
+        title="Choose from the following to switch between decades.">
         {#each DECADES as decade}
             <button
                 on:click={show}
                 key={`button-${decade}`}
                 class="button"
+                role="menuitem"
+                title={`${decade}`}
+                aria-label={`${decade}`}
                 class:active={select_decade == decade}>
                 {decade}
             </button>
         {/each}
     </div>
-    <div class="hide-medium">
-        <label>Decade:</label>
-        <select>
+    <div
+        class="hide-medium"
+        role="menubar"
+        aria-label="Choose from the following to switch between decades."
+        title="Choose from the following to switch between decades.">
+        <label for="decade_nav">Decade:</label>
+        <select id="decade_nav">
             {#each DECADES as decade}
-                <option key={`option-${decade}`} value={decade}>
+                <option key={`option-${decade}`} value={decade} role="menuitem" title={`${decade}`} aria-label={`${decade}`}>
                     {decade}
                 </option>
             {/each}
